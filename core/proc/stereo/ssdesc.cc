b3a623bc973cdca9b7adc6b2e43e2dc53ad9681c
Andrey Myznikov <andrey.myznikov@gmail.com>
2023-04-21 14:30:24 +0300

    Experiments with ssdesc

--- core/proc/stereo/ssdesc.cc
+++ core/proc/stereo/ssdesc.cc
@@ -98,19 +98,26 @@ static inline uint8_t absdiff(const ssdesc & a, const ssdesc & b)
   //  }
   //  return s;
 
+  //  ssdesc c;
+  //  c.u64 = _m_to_int64(_mm_sub_pi8(_mm_max_pu8(ma, mb), _mm_min_pu8(ma, mb)));
+  //  uint8_t s = c.arr[0];
+  //  for( int i = 1; i < 8; ++i ) {
+  //    s = std::max(s, c.arr[i]);
+  //  }
+
   const __m64 ma = _m_from_int64(a.u64);
   const __m64 mb = _m_from_int64(b.u64);
-  ssdesc c;
-  c.u64 = _m_to_int64(_mm_sub_pi8(_mm_max_pu8(ma, mb), _mm_min_pu8(ma, mb)));
 
-//  ssdesc c;
-//  c.m64 = _mm_sub_pi8(_mm_max_pu8(a.m64, b.m64), _mm_min_pu8(a.m64, b.m64));
+  const union {
+    __m64 m;
+    uint8_t arr[8];
+  } u = {
+      .m = _mm_sub_pi8(_mm_max_pu8(ma, mb), _mm_min_pu8(ma, mb))
+  };
 
-  uint8_t s = c.arr[0];
+  uint8_t s = u.arr[0];
   for( int i = 1; i < 8; ++i ) {
-    if( c.arr[i] > s ) {
-      s = c.arr[i];
-    }
+    s = std::max(s, u.arr[i]);
   }
 
   return s;
@@ -216,59 +223,7 @@ void ssdesc_compute(const cv::Mat3b & image, cv::OutputArray & _desc, int flags)
 
   _desc.move(desc);
 }
-//
-//void ssdesc_compute(const cv::Mat3b & image, cv::OutputArray & _desc)
-//{
-//  cv::Mat s;
-//  cv::Mat1f gx, gy, gxx, gyy, gxy;
-//  std::vector<cv::Mat> lab;
-//
-//  static const thread_local cv::Mat1f G = cv::getGaussianKernel(ss_size, ss_sigma, CV_32F) / 16.0;
-//  cv::sepFilter2D(image, s, CV_32F, G, G);
-//
-//  //cv::GaussianBlur(image, s, cv::Size(ss_size, ss_size), ss_sigma, ss_sigma, cv::BORDER_REPLICATE);
-//  //cv::cvtColor(s, s, cv::COLOR_BGR2Lab);
-//  cv::cvtColor(s, s, cv::COLOR_BGR2YCrCb);
-//  cv::split(s, lab);
-//
-//  const cv::Mat1f g = lab[0];
-//  const cv::Mat1f a = lab[1];
-//  const cv::Mat1f b = lab[2];
-//
-//  compute_gradients(g, gx, gy, gxx, gyy, gxy);
-//
-//  cv::Mat4w desc(g.size()/*, cv::Vec4w(0, 0, 0, 0)*/);
-//
-//  tbb::parallel_for(tbb_range(0, g.rows, tbb_grain_size),
-//      [&](const tbb_range & r) {
-//
-//        const int width = g.cols;
-//
-//        constexpr float scale1 = 255.f / 100;
-//        constexpr float scale2 = 5;
-//
-//        for( int y = r.begin(); y < r.end(); ++y ) {
-//
-//          ssdesc *ssp = reinterpret_cast<ssdesc*>(desc[y]);
-//
-//          for( int x = 0; x < width; ++x ) {
-//
-//            ssdesc & ss = ssp[x];
-//            ss.g = (uint8_t) (g[y][x] * scale1);
-//            ss.gx = (uint8_t) (gx[y][x] * scale2 + 128);
-//            ss.gy = (uint8_t) (gy[y][x] * scale2 + 128);
-//            ss.gxx = (uint8_t) (gxx[y][x] * scale2 + 128);
-//            ss.gyy = (uint8_t) (gyy[y][x] * scale2 + 128);
-//            ss.gxy = (uint8_t) (gxy[y][x] * scale2 + 128);
-//            ss.a = (uint8_t) (a[y][x] + 128);
-//            ss.b = (uint8_t) (b[y][x] + 128);
-//          }
-//        }
-//
-//      });
-//
-//  _desc.move(desc);
-//}
+
 
 void ssdesc_cvtfp32(const cv::Mat & _desc, cv::OutputArray output, int flags)
 {

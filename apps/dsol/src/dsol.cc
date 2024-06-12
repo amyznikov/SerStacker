5be9d399f7e7af43e5cb7a5b02feb53fedaf7712
Andrey Myznikov <andrey.myznikov@gmail.com>
2024-06-12 10:35:26 +0300

    Add apps/qdso

--- apps/dsol/src/dsol.cc
+++ apps/dsol/src/dsol.cc
@@ -5,15 +5,11 @@
  *      Author: amyznikov
  */
 //
-//#include "odom.h"
-//#include "extra.h"
-
 #include "dso/FullSystem.h"
 #include "dso/c_dso_dataset_reader.h"
 #include "c_dso_display_opencv.h"
 #include <core/settings.h>
 #include <core/debug.h>
-
 
 using namespace dso;
 

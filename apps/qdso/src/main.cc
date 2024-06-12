6e2759643782aa36653d58f6d53fd269041b5482
Andrey Myznikov <andrey.myznikov@gmail.com>
2024-06-12 12:30:17 +0300

    Update cmake build scripts

--- apps/qdso/src/main.cc
+++ apps/qdso/src/main.cc
@@ -6,14 +6,14 @@
  */
 #include <opencv2/core/ocl.hpp>
 #include <gui/widgets/style.h>
-//#include "MainWindow.h"
+#include "MainWindow.h"
 #include <core/debug.h>
 
 
 #define MY_COMPANY  "amyznikov"
 #define MY_APP      "qdso"
 
-//using namespace serstacker;
+using namespace qdso;
 
 int main(int argc, char *argv[])
 {
@@ -21,27 +21,27 @@ int main(int argc, char *argv[])
   // Comment out this line if you want to allow OCL in OpenCV
   cv::ocl::setUseOpenCL(false);
 
-//  QApplication app(argc, argv);
-//
-//  app.setOrganizationName(MY_COMPANY);
-//  app.setApplicationName(MY_APP);
-//
-//  Q_INIT_RESOURCE(app_resources);
-//  Q_INIT_RESOURCE(gui_resources);
-//
-//  cf_set_logfile(stderr);
-//  cf_set_loglevel(CF_LOG_DEBUG);
-//
-//  setIconStyleSelector("dark");
-//
-//
-//  MainWindow mainWindow;
-//  mainWindow.show();
-//  mainWindow.activateWindow();
-//  mainWindow.raise();
-//
-//
-//  return app.exec();
+  QApplication app(argc, argv);
+
+  app.setOrganizationName(MY_COMPANY);
+  app.setApplicationName(MY_APP);
+
+  Q_INIT_RESOURCE(app_resources);
+  Q_INIT_RESOURCE(gui_resources);
+
+  cf_set_logfile(stderr);
+  cf_set_loglevel(CF_LOG_DEBUG);
+
+  setIconStyleSelector("dark");
+
+
+  MainWindow mainWindow;
+  mainWindow.show();
+  mainWindow.activateWindow();
+  mainWindow.raise();
+
+
+  return app.exec();
 
   return 0;
 }

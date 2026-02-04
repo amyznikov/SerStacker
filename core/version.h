47dfe7690972ef3c3c1561df94a5885364ff2a0f
Andrey Myznikov <andrey.myznikov@gmail.com>
2026-02-04 17:58:25 +0200

    Update cmake scripts

--- core/version.h
+++ core/version.h
@@ -5,13 +5,13 @@
 #define PROJECT_VERSION_MAJOR 1
 #define PROJECT_VERSION_MINOR 1
 #define PROJECT_VERSION_PATCH 2
-#define PROJECT_GIT_HASH     "10dd48d"
+#define PROJECT_GIT_HASH     "873ced3"
 
 #define PROJECT_VERSION_INT(major, minor, patch) ((major << 16) | (minor << 8) | patch)
 #define PROJECT_VERSION_CURR PROJECT_VERSION_INT(PROJECT_VERSION_MAJOR, PROJECT_VERSION_MINOR, PROJECT_VERSION_PATCH)
 
 #define PROJECT_VERSION "1.1.2"
-#define PROJECT_FULL_VERSION_STR "1.1.2-10dd48d"
+#define PROJECT_FULL_VERSION_STR "1.1.2-873ced3"
 
 #endif // __project_version_h__
 

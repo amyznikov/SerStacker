76ab8db389fd63ddf4e5db2294517b206f339f6b
Andrey Myznikov <andrey.myznikov@gmail.com>
2026-03-04 15:57:03 +0200

    Update edge map computetion for c_adaptive_gaussian_blur_routine

--- core/improc/generic/c_adaptive_gaussian_blur_routine.cc
+++ core/improc/generic/c_adaptive_gaussian_blur_routine.cc
@@ -35,6 +35,7 @@ const c_enum_member * members_of<c_adaptive_gaussian_blur_routine::DisplayType>(
 void c_adaptive_gaussian_blur_routine::getcontrols(c_control_list & ctls, const ctlbind_context & ctx)
 {
   ctlbind(ctls, "seRadius", ctx(&this_class::seRadius), "Morphologcal SE radius");
+  ctlbind_spinbox(ctls, "edgeLimit", ctx(&this_class::edgeLimit), 0.0, 100.0, 0.01, "");
   ctlbind(ctls, "mkRadius", ctx(&this_class::mkRadius), "medianBlur kernel radius for guide image");
   ctlbind(ctls, "gfSigma", ctx(&this_class::gfSigma), "Gaussian filter sigma");
   ctlbind(ctls, "hBoost", ctx(&this_class::hBoost), "");
@@ -50,7 +51,7 @@ bool c_adaptive_gaussian_blur_routine::serialize(c_config_setting settings, bool
   if( base::serialize(settings, save) ) {
     SERIALIZE_OPTION(settings, save, *this, displayType);
     SERIALIZE_OPTION(settings, save, *this, seRadius);
-//    SERIALIZE_OPTION(settings, save, *this, lpgK);
+    SERIALIZE_OPTION(settings, save, *this, edgeLimit);
 //    SERIALIZE_OPTION(settings, save, *this, lpgP);
 //    SERIALIZE_OPTION(settings, save, *this, lpgDscale);
 //    SERIALIZE_OPTION(settings, save, *this, lpgUscale);
@@ -64,6 +65,46 @@ bool c_adaptive_gaussian_blur_routine::serialize(c_config_setting settings, bool
     return true;
   }
   return false;
+}
+
+static double calculateXMax(const cv::Mat1f & src, double percentile = 0.95)
+{
+  std::vector<float> samples;
+  if( src.isContinuous() ) {
+    samples.assign(src.ptr<float>(0), src.ptr<float>(0) + src.total());
+  }
+  else {
+    for( int i = 0; i < src.rows; ++i ) {
+      samples.insert(samples.end(), src.ptr<float>(i), src.ptr<float>(i) + src.cols);
+    }
+  }
+
+  const auto it = samples.begin() + static_cast<int>((samples.size() - 1) * std::max(0.0, std::min(percentile, 1.0)));
+  std::nth_element(samples.begin(), it, samples.end());
+
+  return static_cast<double>(*it);
+}
+
+static void mapPixels(cv::InputArray _src, cv::OutputArray _dst, double xmax)
+{
+  const cv::Mat1f src = _src.getMat();
+  _dst.create(src.size(), src.type());
+  cv::Mat1f dst = _dst.getMatRef();
+
+  const float n = 6.0f;
+  const float xmax_n = std::pow(static_cast<float>(xmax), n);
+  const float inv_n = 1.0f / n;
+
+  dst.forEach([&](float & pixel, const int position[]) -> void {
+    const float x = src(position[0], position[1]);
+    if (x <= 0.0f) {
+      pixel = 0.0f;
+    }
+    else { // pixel = x / (x^n + xmax^n)^(1/n)
+      float x_n = std::pow(x, n);
+      pixel = x / std::pow(x_n + xmax_n, inv_n);
+    }
+  });
 }
 
 bool c_adaptive_gaussian_blur_routine::process(cv::InputOutputArray image, cv::InputOutputArray mask)
@@ -102,14 +143,9 @@ bool c_adaptive_gaussian_blur_routine::process(cv::InputOutputArray image, cv::I
     }
 
 
-    cv::divide(ramp, tex, edges);
 
-    cv::Scalar m, s;
-    cv::meanStdDev(edges, m, s);
-    cv::multiply(edges, -1.0 / (m[0] + 5 * s[0]), edges);
-    //cv::multiply(edges, edges, edges, -1.0 / (m[0] + 3 * s[0]));
-    cv::exp(edges, edges);
-    cv::subtract(1.0f, edges, edges);
+    cv::divide(ramp, tex, edges);
+    mapPixels(edges, edges, calculateXMax(edges, 0.01 * edgeLimit));
 
     if( displayType == DisplayEdges ) {
       edges.copyTo(image);
--- core/improc/generic/c_adaptive_gaussian_blur_routine.h
+++ core/improc/generic/c_adaptive_gaussian_blur_routine.h
@@ -16,7 +16,7 @@ class c_adaptive_gaussian_blur_routine :
 {
 public:
   DECLATE_IMAGE_PROCESSOR_CLASS_FACTORY(c_adaptive_gaussian_blur_routine,
-      "c_adaptive_blend_filter_routine", "Apply adaptive blend denoise filter");
+      "c_adaptive_gaussian_blur_routine", "Apply adaptive denoise filter");
 
   enum DisplayType {
     DisplayFiltered,
@@ -42,17 +42,17 @@ protected:
   cv::Mat weights;
   cv::Mat detail;
 
-  DisplayType displayType = DisplayFiltered;
-
   int seRadius = 2;
-
   int mkRadius = 2;
+  double edgeLimit = 99;
   double gfSigma = 1.5;
   double hBoost = 1;
   float x1 = 0.1f;
   float a1 = 0.2f;
   float x2 = 0.9f;
   float a2 = 0.5f;
+
+  DisplayType displayType = DisplayFiltered;
 };
 
 #endif /* __c_adaptive_blend_filter_routine_h__ */

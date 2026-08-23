e0f6a326c244f1f1e3d86cf6ceac0ad70d821a0f
Andrey Myznikov <andrey.myznikov@gmail.com>
2026-08-23 14:24:40 +0300

    Update c_crop_image_routine

--- core/improc/geometry/c_crop_image_routine.cc
+++ core/improc/geometry/c_crop_image_routine.cc
@@ -6,32 +6,54 @@
  */
 
 #include "c_crop_image_routine.h"
+#include <core/ssprintf.h>
 
-//static void adjust_rect(const cv::Rect & src, cv::Rect & dst, const cv::Size & image_size)
-//{
-//  const int l = std::max(0, std::min(image_size.width - 1, src.x));
-//  const int t = std::max(0, std::min(image_size.height - 1, src.y));
-//  const int r = std::max(0, std::min(image_size.width - 1, src.x + src.width - 1));
-//  const int b = std::max(0, std::min(image_size.height - 1, src.y + src.height - 1));
-//
-//  dst.x = l;
-//  dst.y = t;
-//  dst.width = r >= 0 ? r - l + 1 : -1;
-//  dst.height = b >= 0 ? b - t + 1 : -1;
-//}
+template<>
+const c_enum_member * members_of<c_crop_image_routine::SELECTION_MODE>()
+{
+  static const c_enum_member members[] = {
+      { c_crop_image_routine::ROI_SELECTION_NONE,"NONE", ""},
+      { c_crop_image_routine::ROI_SELECTION_FIXED_RECT,"RECT", ""},
+      { c_crop_image_routine::ROI_SELECTION_GUI, "GUI", "" },
+      { c_crop_image_routine::ROI_SELECTION_PLANETARY_DISK, "PLANETARY_DISK", "" },
+//      { c_crop_image_routine::ROI_SELECTION_MAXCC, "MAXCC", "" },
+      { c_crop_image_routine::ROI_SELECTION_GUI}
+  };
+  return members;
+}
 
 
 void c_crop_image_routine::getcontrols(c_control_list & ctls, const ctlbind_context & ctx)
 {
-  ctlbind(ctls, "Use ROI selection", ctx(&this_class::_useROISelection), "Use ROI selected in GUI");
-  ctlbind(ctls, "rect", ctx(&this_class::_rect), "Rectangular ROI to crop, X,Y;WxH");
+  ctlbind(ctls, "Selection", CTL_CONTEXT(ctx, mode), "What to select into ROI");
+
+  ctlbind(ctls, "fixed output size:", CTL_CONTEXT(ctx, fixOutputSize), "");
+  ctlbind(ctls, "output size:", CTL_CONTEXT(ctx, outputSize), "");
+
+  ctlbind_expandable_group(ctls, "RECT", [&, ctx = CTL_CONTEXT(ctx, fixed_rect_options)]() {
+    ctlbind(ctls, "ROI:", CTL_CONTEXT(ctx, rc), "");
+  });
+
+  ctlbind_expandable_group(ctls, "PLANETARY DISK", [&, ctx = CTL_CONTEXT(ctx, planetary_disk_options)]() {
+    ctlbind(ctls, CTL_CONTEXT(ctx, opts));
+  });
+
 }
 
 bool c_crop_image_routine::serialize(c_config_setting settings, bool save)
 {
   if( base::serialize(settings, save) ) {
-    SERIALIZE_OPTION(settings, save, *this, _rect);
-    SERIALIZE_OPTION(settings, save, *this, _useROISelection);
+    SERIALIZE_OPTION(settings, save, *this, mode);
+    SERIALIZE_OPTION(settings, save, *this, fixOutputSize);
+    SERIALIZE_OPTION(settings, save, *this, outputSize);
+
+    if ( auto group = SERIALIZE_GROUP(settings, save, "fixed_rect_options") ) {
+      SERIALIZE_OPTION(group, save, fixed_rect_options, rc );
+    }
+
+    if( auto group = SERIALIZE_GROUP(settings, save, "planetary_disk_options") ) {
+      serialize_base_planetary_disk_detector_options(group, save, planetary_disk_options.opts);
+    }
 
     return true;
   }
@@ -42,28 +64,94 @@ bool c_crop_image_routine::process(cv::InputOutputArray image, cv::InputOutputAr
 {
   const cv::Size size = image.size();
 
-  cv::Rect rc;
-  if ( !_useROISelection ) {
-    rc = _rect;
+  cv::Rect roi;
+
+  bool fOk = false;
+
+  switch (mode) {
+    case ROI_SELECTION_NONE: {
+      return true;
+    }
+    case ROI_SELECTION_FIXED_RECT: {
+      fOk = !(roi = fixed_rect_options.rc).empty();
+      break;
+    }
+    case ROI_SELECTION_GUI: {
+      fOk = ctlbind_get_roi(&roi);
+      break;
+    }
+    case ROI_SELECTION_PLANETARY_DISK: {
+      fOk = simple_planetary_disk_detector(image, mask,
+          planetary_disk_options.opts,
+          nullptr,
+          &roi,
+          nullptr,
+          nullptr,
+          cv::noArray());
+
+      if ( fOk && fixOutputSize && !outputSize.empty()) {
+        // Expand roi to outputSize still centered
+        cv::Point center(roi.x + roi.width / 2, roi.y + roi.height / 2);
+        roi.x = center.x - outputSize.width / 2;
+        roi.y = center.y - outputSize.height / 2;
+        roi.width = outputSize.width;
+        roi.height = outputSize.height;
+        roi &= cv::Rect(0, 0, size.width, size.height);
+      }
+
+      break;
+    }
+
+    default:
+      CF_ERROR("Bad selection method = %d", mode);
+      return false;
   }
-  else if ( !ctlbind_get_roi(&rc) ) {
-    CF_ERROR("ROI is not selected");
+
+  if( !fOk || (roi &= cv::Rect(0, 0, size.width, size.height)).empty() ) {
+    CF_ERROR("The selection ROI is empty or target object not detected");
     return false;
   }
 
-  rc &= cv::Rect(0, 0, size.width, size.height);
-
-  //adjust_rect(_rect, rc, size);
-
-  if ( rc.empty() ) {
-    CF_ERROR("c_crop_image_routine: adjusted rect is empty");
+  if ( !fixOutputSize || outputSize.empty() ) {
+    image.getMat()(roi).copyTo(image);
+    if ( !mask.empty() )  {
+      mask.getMat()(roi).copyTo(mask);
+    }
   }
   else {
-    if( image.needed() && !image.empty() ) {
-      image.getMat()(rc).copyTo(image);
+    // Copy image.getMat()(roi) to outimage(outroi) centered
+    cv::Mat outimage(outputSize, image.type(), cv::Scalar::all(0));
+    cv::Mat outmask;
+
+    const int dx = (outputSize.width - roi.width + 1) / 2;
+    const int dy = (outputSize.height - roi.height + 1) / 2;
+
+    const cv::Rect dst_rect(dx, dy, roi.width, roi.height);
+    const cv::Rect dst_clipped = dst_rect & cv::Rect(0, 0, outputSize.width, outputSize.height);
+    if (dst_clipped.empty()) {
+      CF_ERROR("dst_clipped is empty");
+      return false;
     }
-    if( mask.needed() && !mask.empty() ) {
-      mask.getMat()(rc).copyTo(mask);
+
+    roi.x += (dst_clipped.x - dst_rect.x);
+    roi.y += (dst_clipped.y - dst_rect.y);
+    roi.width = dst_clipped.width;
+    roi.height = dst_clipped.height;
+
+    image.getMat()(roi).copyTo(outimage(dst_clipped));
+
+    if (!mask.empty()) {
+      outmask = cv::Mat::zeros(outputSize, mask.type());
+      mask.getMat()(roi).copyTo(outmask(dst_clipped));
+    }
+    else if (dst_clipped.size() != outputSize) {
+      outmask = cv::Mat::zeros(outputSize, CV_8UC1);
+      outmask(dst_clipped).setTo(cv::Scalar::all(255));
+    }
+
+    image.move(outimage);
+    if (!outmask.empty()) {
+      mask.move(outmask);
     }
   }
 
--- core/improc/geometry/c_crop_image_routine.h
+++ core/improc/geometry/c_crop_image_routine.h
@@ -10,6 +10,9 @@
 #define __c_crop_image_routine_h__
 
 #include <core/improc/c_image_processor.h>
+#include <core/proc/feature2d/c_roi_selection.h>
+#include <core/proc/feature2d/c_planetary_disk_selection.h>
+#include <core/proc/feature2d/planetary-disk-detection.h>
 
 class c_crop_image_routine :
     public c_image_processor_routine
@@ -18,13 +21,32 @@ public:
   DECLATE_IMAGE_PROCESSOR_CLASS_FACTORY(c_crop_image_routine,
       "crop", "Crop rectangle region of image");
 
+  enum SELECTION_MODE {
+    ROI_SELECTION_NONE,
+    ROI_SELECTION_FIXED_RECT,
+    ROI_SELECTION_GUI,
+    ROI_SELECTION_PLANETARY_DISK,
+//    ROI_SELECTION_MAXCC,
+  };
+
   bool serialize(c_config_setting settings, bool save) final;
   bool process(cv::InputOutputArray image, cv::InputOutputArray mask = cv::noArray()) final;
   static void getcontrols(c_control_list & ctls, const ctlbind_context & ctx);
 
 protected:
-  cv::Rect _rect;
-  bool _useROISelection = false;
+  SELECTION_MODE mode = ROI_SELECTION_GUI;
+  cv::Size outputSize;
+  bool fixOutputSize = false;
+
+  struct {
+    cv::Rect rc;
+  } fixed_rect_options;
+
+  struct {
+    c_simple_planetary_disk_detector_options opts;
+    bool center = false;
+  } planetary_disk_options;
+
 };
 
 #endif /* __c_crop_image_routine_h__ */
